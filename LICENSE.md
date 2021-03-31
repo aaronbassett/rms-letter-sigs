0b577862211ba395961cd3517d197420f3c24108
aspen <aspenuwu@protonmail.com>
2021-03-31 10:16:21 -0400

    Suggest a proper license.

---
+++ LICENSE.md
@@ -0,0 +1,11 @@
+# The "Anyone But Richard M Stallman" license
+
+You are permitted to do anything you wish with this software, with the exceptions listed below under "EXCEPTIONS".
+
+THIS SOFTWARE IS PROVIDED "AS IS" WITH NO WARRANTY OF ANY KIND.
+
+# EXCEPTIONS
+
+This software or any deriatives may not be used by Richard M. Stallman (the founder of the GNU Project and the Free Software Foundation), or in any activities intended to support the Free Software Foundation or Richard M. Stallman.
+
+This software may *not* be relicensed.
--- content.js
+++ content.js
@@ -1,3 +1,14 @@
+/*
+ You are permitted to do anything you wish with this software, 
+ with the exceptions listed below under "EXCEPTIONS".
+ THIS SOFTWARE IS PROVIDED "AS IS" WITH NO WARRANTY OF ANY KIND.
+
+ EXCEPTIONS
+ This software or any deriatives may not be used by 
+ Richard M. Stallman (the founder of the GNU Project and the Free Software Foundation), 
+ or in any activities intended to support the Free Software Foundation or Richard M. Stallman.
+*/
+
 let pageHeaderActions = document.querySelector(".pagehead-actions")
 const actionLinkClasses = ["btn", "btn-sm", "btn-danger"]
 const githubUsernames = [
